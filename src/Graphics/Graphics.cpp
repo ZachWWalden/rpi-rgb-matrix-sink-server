b68d27443be21cf972f6654a9f85634956ecfa16
Zachary Walden <zachary.walden@eagles.oc.edu>
2025-03-25 01:24:13 -0500

    reformat canvas drawing functions for readability

--- src/Graphics/Graphics.cpp
+++ src/Graphics/Graphics.cpp
@@ -79,7 +79,10 @@ void Graphics::draw()
 	{
 		for(int cols = 0; cols < this->width; cols++)
 		{
-			this->SetCanvasPixel(cols, rows, Color(0xFF, this->render_target[rows][cols][0], this->render_target[rows][cols][1], this->render_target[rows][cols][2]));
+			this->SetCanvasPixel(cols, rows, Color(0xFF,	this->render_target[rows][cols][0],
+															this->render_target[rows][cols][1],
+															this->render_target[rows][cols][2])
+												  );
 		}
 	}
 }
@@ -108,7 +111,10 @@ void Graphics::drawWithMaps(std::vector<ZwConfig::PanelMap*>* panels)
 			for(int cols = (*itr)->destination.p_top_left.y ;cols <= (*itr)->destination.p_bot_right.y; cols++)
 			{
 				//Write pixel to canvas
-				this->SetCanvasPixel(cols, rows, Color(0xFF, this->render_target[y][x][0], this->render_target[y][x][1], this->render_target[y][x][2]));
+				this->SetCanvasPixel(cols, rows, Color(0xFF,	this->render_target[y][x][0],
+																this->render_target[y][x][1],
+																this->render_target[y][x][2])
+													   );
 				if((*itr)->rot_constants.row_major)
 					x += (*itr)->rot_constants.increment.x;
 				else
