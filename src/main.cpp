0c97b373ab1613537d11d9dd9ff9304c33012c3a
Zachary Walden <zachary.walden@eagles.oc.edu>
2025-07-02 06:00:36 -0500

    fix: small memory leak

--- src/main.cpp
+++ src/main.cpp
@@ -118,6 +118,7 @@ int main(int argc, char *argv[]) {
 				LOG("MSG Received");
 				ZwNetwork::SinkPacket *msg = (ZwNetwork::SinkPacket*) buf;
 				graphics_mgr->setRenderTarget(graphics_mgr->convertFlatBufferToTriplePointer(*msg));
+				free(buf);
 				LOG("Buffer converted");
 				//When a frame is received, map each of it's regions to a panel in the chain. And draw to canvas
 				graphics_mgr->drawWithMaps(config->getPanelMaps());
