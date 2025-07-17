a12a0fe779483be81bca36adac28a72d321a9021
Zachary Walden <zachary.walden@eagles.oc.edu>
2025-07-16 20:12:57 -0500

    test: change datatype for critical variables

--- src/Config/Config.hpp
+++ src/Config/Config.hpp
@@ -92,8 +92,8 @@ class Config
 	//Attributes
 public:
 	uint16_t hres, vres, panel_hres, panel_vres, port;
-	uint8_t num_chains;
-	uint8_t chain_length;
+	uint16_t num_chains;
+	uint16_t chain_length;
 	bool is_valid = false;
 
 private:
